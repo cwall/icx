05f06cf408cd2d947eb1c0aa6fbb0a8bb8c2ab0f
Carl Waldron <cwaldron@freelancersunion.org>
2016-12-23 12:04:34 -0500

    brunched up cx folders

---
+++ brunch-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // paths: {
+  //   public: 'static',
+  //   watched: [
+  //     'app',
+  //     'assets',
+  //     'styles',
+  //     'vendor',
+  //   ]
+  // },
+  files: {
+    javascripts: {
+      joinTo: {
+        'js/full.js': /^app(\/|\\)js/,
+        'js/vendor.js': /^(vendor|bower_components)/,
+        'test/js/test.js': /^test(\/|\\)(?!vendor)/,
+        'test/js/test-vendor.js': /^test(\/|\\)(?=vendor)/
+      }
+    },
+    stylesheets: {
+      joinTo: {
+        'css/cx.css': /^app/,
+        'css/vendor.css': /^(vendor|bower_components)/
+      }
+    },
+    templates: {joinTo: 'full.js'}
+  },
+  paths: {
+    public: 'static'
+  }
+}
